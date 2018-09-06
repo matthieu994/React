5c3586ed7994044b7ceab77190c5aafd10d5bcee
Matthieu <Matthieu-Xiaomi@Ubuntu>
2018-09-06 13:54:17 -0400

    arrow keys final

--- react-ui/src/Live/Live.js
+++ react-ui/src/Live/Live.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Live.css'
+import { ToastContainer, toast } from 'react-toastify';
+import { Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import io from 'socket.io-client';
 
@@ -19,8 +21,13 @@ export default class Live extends Component {
             this.setState({ code })
         })
 
-        this.socket.on('linkDone', () => {
+        this.socket.on('linkDone', (res) => {
             this.setState({ linked: true })
+            console.log(res)
+        })
+
+        this.socket.on('noRoom', message => {
+            toast.error(message)
         })
     }
     componentWillUnmount() {
@@ -34,7 +41,8 @@ export default class Live extends Component {
         this.setState({ mobile: true })
     }
 
-    enterCode() {
+    enterCode(e) {
+        e.preventDefault()
         if (!this.state.code) return
         this.socket.emit('link', this.state.code)
     }
@@ -49,13 +57,13 @@ export default class Live extends Component {
         }
         if (this.state.mobile && !this.state.linked) {
             input =
-                <div>
+                <form>
                     <input className="form-control" value={this.state.code}
-                        onChange={(e) => this.setState({ code: e.target.value })} />
+                        onChange={(e) => this.setState({ code: e.target.value.toUpperCase() })} />
                     <button className="btn btn-outline-primary ml-2"
-                        onClick={() => { this.enterCode() }}
+                        onClick={e => { this.enterCode(e) }}
                     >Lier</button>
-                </div>
+                </form>
         }
         if (this.state.linked) {
             input = null;
@@ -77,7 +85,16 @@ export default class Live extends Component {
                         {(this.state.code || this.state.mobile) && input}
                     </div>
                 </div>
-                {this.state.linked && <Game code={this.state.code} mobile={this.state.mobile} socket={this.socket} />}
+                {this.state.linked && !this.state.mobile && <Game code={this.state.code} mobile={this.state.mobile} socket={this.socket} />}
+                {this.state.linked && this.state.mobile && <Commands code={this.state.code} mobile={this.state.mobile} socket={this.socket} />}
+                <ToastContainer
+                    position="top-center"
+                    autoClose={2500}
+                    hideProgressBar={true}
+                    rtl={false}
+                    pauseOnHover={false}
+                    transition={Zoom}
+                />
             </div>
         )
     }
@@ -90,18 +107,79 @@ class Game extends Component {
 
     componentDidMount() {
         this.socket = this.props.socket;
-        this.socket.on('updatePos', pos => {
-            this.setState({ direction: pos })
+        this.socket.on('updatePos', direction => {
+            this.setState({ direction })
         })
+    }
+
+    render() {
+        return (
+            <div className="live-game">
+                <Image direction={this.state.direction} />
+            </div>
+        )
+    }
+}
+
+class Image extends Component {
+    state = {
+        x: 0,
+        y: 0,
+    }
+
+    componentWillReceiveProps(newProps) {
+        // console.log(newProps)
+        if (newProps.direction === 'left')
+            this.setState({ x: this.state.x - 10 })
+        if (newProps.direction === 'right')
+            this.setState({ x: this.state.x + 10 })
+        if (newProps.direction === 'up')
+            this.setState({ y: this.state.y - 10 })
+        if (newProps.direction === 'down')
+            this.setState({ y: this.state.y + 10 })
+    }
+
+    render() {
+        let style = {
+            left: this.state.x,
+            top: this.state.y
+        }
+        return (
+            <img style={style} src={SPRITE} alt='character'></img>
+        )
+    }
+}
+
+class Commands extends Component {
+    state = {
+        alpha: null,
+        beta: null,
+        gamma: null
+    }
+
+    componentDidMount() {
+        this.socket = this.props.socket;
+        this.handleOrientation()
+        this.beat = setInterval(() => {
+            this.socket.emit('updatePos', {
+                code: this.props.code,
+                beta: this.state.beta,
+                gamma: this.state.gamma
+            })
+        }, 100)
+    }
+    handleOrientation() {
+        window.addEventListener("deviceorientation", e => {
+            this.setState({ alpha: e.alpha, beta: e.beta, gamma: e.gamma })
+        }, true);
     }
 
     move(direction) {
         this.socket.emit('updatePos', {
             code: this.props.code,
-            direction: direction
+            direction
         })
     }
-
     moveDebug(e) {
         let dir;
         if (e.keyCode === 37)
@@ -120,47 +198,16 @@ class Game extends Component {
 
     render() {
         return (
-            <div className="live-game">
-                {this.props.mobile &&
-                    <div>
-                        <button onClick={() => this.move('left')}>Gauche</button>
-                        <button onClick={() => this.move('right')}>Droite</button>
-                        <button onClick={() => this.move('up')}>Haut</button>
-                        <button onClick={() => this.move('down')}>Bas</button>
-                        <input placeholder="debug" type="text" onKeyDown={e => this.moveDebug(e)} />
-                    </div>
-                }
-                {!this.props.mobile && <Image direction={this.state.direction} />}
+            <div>
+                <div>
+                    <button onClick={() => this.move('left')}>Gauche</button>
+                    <button onClick={() => this.move('right')}>Droite</button>
+                    <button onClick={() => this.move('up')}>Haut</button>
+                    <button onClick={() => this.move('down')}>Bas</button>
+                    <input placeholder="debug" type="text" onKeyDown={e => this.moveDebug(e)} />
+                </div>
+                {this.state.alpha + ' ' + this.state.beta + ' ' + this.state.gamma}
             </div>
-        )
-    }
-}
-
-class Image extends Component {
-    state = {
-        x: 0,
-        y: 0
-    }
-
-    componentWillReceiveProps(newProps) {
-        console.log(newProps)
-        if (newProps.direction === 'left')
-            this.setState({ x: this.state.x - 10 })
-        if (newProps.direction === 'right')
-            this.setState({ x: this.state.x + 10 })
-        if (newProps.direction === 'up')
-            this.setState({ y: this.state.y - 10 })
-        if (newProps.direction === 'down')
-            this.setState({ y: this.state.y + 10 })
-    }
-
-    render() {
-        let style = {
-            left: this.state.x,
-            top: this.state.y
-        }
-        return (
-            <img style={style} src={SPRITE} alt='character'></img>
         )
     }
 }
--- server/socket.io/Live.js
+++ server/socket.io/Live.js
@@ -3,22 +3,38 @@ module.exports = function (io) {
     lio.on('connection', function (client) {
         client.on('createCode', () => {
             const code = generateCode()
+            if (usersCount(io, code) != 0)
+                return noRoom(client, code);
             client.emit('getCode', code)
             client.join(code);
         })
-        
+
+
         client.on('link', code => {
+            if (usersCount(io, code) != 1)
+                return noRoom(client, code)
             client.join(code);
             lio.in(code).emit('linkDone', "Link réussi !");
         })
 
         client.on('updatePos', data => {
-            console.log(data)
-            client.broadcast.to(data.code).emit('updatePos', data.direction);
+            if (data.direction)
+                client.broadcast.to(data.code).emit('updatePos', data.direction);
+            else
+                client.broadcast.to(data.code).emit('updatePos', { X: data.beta, Y: data.gamma });
         })
     });
 }
 
 const generateCode = function () {
     return Math.random().toString(36).substring(2, 7).toUpperCase();
+}
+
+const usersCount = function (io, room) {
+    if (!io.nsps['/Live'].adapter.rooms[room]) return 0
+    return io.nsps['/Live'].adapter.rooms[room].length
+}
+
+const noRoom = function (client) {
+    client.emit('noRoom', "Ce code n'est pas valide");
 }
