430d29d94b98ada3764f569172516df4d8490118
Matthieu <Matthieu-Xiaomi@Ubuntu>
2018-10-07 03:16:53 -0400

    .

--- server/Auth/Tools.js
+++ server/Auth/Tools.js
@@ -3,9 +3,9 @@ var User = require("../models/UserSchema");
 
 module.exports = {
 	getUser: async function(token, res, props) {
-		return await jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-			if (err) return res.sendStatus(403);
-			console.log(decoded)
+		return await jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+			if (!token || err) return res.sendStatus(403);
+			console.log(decoded);
 			return User.findById(decoded.user, props, (err, user) => {
 				if (err) res.sendStatus(403);
 				return user;
