dd85a878eb5835aa6f6a9bb89a4508fb1acd2d51
Matthieu <Matthieu-Xiaomi@Ubuntu>
2018-11-06 14:12:31 -0500

    display nightshift

--- react-ui/src/Components/Footer/index.js
+++ react-ui/src/Components/Footer/index.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import apps from '../apps.json'
-import "./footer.scss"
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import NightShift from "../NightShift"
+import apps from "../apps.json";
+import "./footer.scss";
 
 class Footer extends Component {
-    render() {
-        return (
-            <footer>
-                <div className="container-fluid text-center">
-                    <div className="row">
-                        <h3 className="col font-weight-bold">Applications</h3>
-                    </div>
-                    <div className="row">
-                        {
-                            apps.map((app, index) =>
-                                <div className="col" key={index}>
-                                    <Link to={app.Component}>{app.title}</Link>
-                                </div>
-                            )
-                        }
-                    </div>
-                </div>
-            </footer>
-        )
-    }
+	render() {
+		return (
+			<footer>
+				<NightShift />
+				<div className="container-fluid text-center">
+					<div className="row">
+						<h3 className="col font-weight-bold">Applications</h3>
+					</div>
+					<div className="row">
+						{apps.map((app, index) => (
+							<div className="col" key={index}>
+								<Link to={app.Component}>{app.title}</Link>
+							</div>
+						))}
+					</div>
+				</div>
+			</footer>
+		);
+	}
 }
-export default withRouter(Footer)
+export default withRouter(Footer);
---
+++ react-ui/src/Components/NightShift/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Button, Fa } from "mdbreact";
+import "./index.scss";
+
+export default class NightShift extends Component {
+	state = {
+		on: localStorage.getItem("nightshift")
+	};
+
+	toggle() {
+		this.setState({ on: !this.display() }, () => {
+			localStorage.setItem("nightshift", this.state.on);
+		});
+	}
+
+	display() {
+		if (this.state.on === true || this.state.on === "true") return true;
+		else return false;
+	}
+
+	render() {
+		return (
+			<Button
+				className="nightshift"
+				color={this.display() ? "indigo" : ""}
+				onClick={() => this.toggle()}>
+				<Fa icon="moon" />
+			</Button>
+		);
+	}
+}
