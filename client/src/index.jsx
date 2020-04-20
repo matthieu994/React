e6f9c62c27a31bb680dc77107c05013e254658a7
Matthieu <matthieu@notebook>
2020-04-20 23:15:17 +0200

    update build script

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,32 +1,31 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-//eslint-disable-next-line
-import index from "./redux/index";
-import store from "./redux/store/index";
+// eslint-disable-next-line
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./redux/store/index";
 import Routes from "./Routes";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
-import { BrowserRouter as Router } from "react-router-dom";
 import "font-awesome/css/font-awesome.min.css";
 // import 'bootstrap-css-only/css/bootstrap.min.css';
 import "mdbreact/dist/css/mdb.css";
 import "./index.css";
 
 class Root extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<Router>
-					<div>
-						<Header />
-						<Routes />
-						<Footer />
-					</div>
-				</Router>
-			</Provider>
-		);
-	}
+    render() {
+        return (
+          <Provider store={store}>
+            <Router>
+              <div>
+                <Header />
+                <Routes />
+                <Footer />
+              </div>
+            </Router>
+          </Provider>
+        );
+    }
 }
 
 render(<Root />, document.getElementById("root"));
