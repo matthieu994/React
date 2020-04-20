76131fd32945ad3d32c579dba15f2a1abe1e11f4
Matthieu <matthieu@notebook>
2020-04-20 23:28:28 +0200

    remove jwt from index

--- server/index.js
+++ server/index.js
@@ -1,27 +1,25 @@
-"use strict";
 
-const express = require("express");
-const path = require("path");
-const jwt = require("jsonwebtoken");
-const tools = require("./tools");
+const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-const bodyParser = require("body-parser");
-require("dotenv").config();
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Mongoose
-const mongoose = require("mongoose");
-var Todo = require("./models/TodoSchema");
-var User = require("./models/UserSchema");
-var models = require("./models/ChatSchema");
+const mongoose = require('mongoose');
+// const tools = require('./tools');
+// const Todo = require('./models/TodoSchema');
+// const User = require('./models/UserSchema');
+// const models = require('./models/ChatSchema');
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 // app.use("/Portfolio", (req, res) => {
 //     express.static(path.resolve(__dirname, "../client/public/PortfolioSrc/"));
 // });
@@ -30,38 +28,37 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 let URL;
 URL = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`;
 
-mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
-    if (err) console.log(err);
-    // tools.removeCollection(User);
-    // tools.showCollections(db);
-    // tools.deleteAllTokens(User);
-    // tools.show(db, 'conversations', {});
-    // tools.removeFriends(User, 'admin')
-    // tools.removeAllFriends(User)
-    // tools.showFriends(User, 'admin')
-    // db.collection("users").remove({ username: "abc" });
-    // db.collection("conversations").remove({});
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+  if (err) console.log(err);
+  // tools.removeCollection(User);
+  // tools.showCollections(db);
+  // tools.deleteAllTokens(User);
+  // tools.show(db, 'conversations', {});
+  // tools.removeFriends(User, 'admin')
+  // tools.removeAllFriends(User)
+  // tools.showFriends(User, 'admin')
+  // db.collection("users").remove({ username: "abc" });
+  // db.collection("conversations").remove({});
 });
-mongoose.set("useCreateIndex", true);
+mongoose.set('useCreateIndex', true);
 
 // Todos
-require("./TodoList/TodoList")(app);
+require('./TodoList/TodoList')(app);
 
 // User management
-require("./Auth/Auth")(app);
+require('./Auth/Auth')(app);
 
 // Profile management
-require("./Auth/Profile")(app);
+require('./Auth/Profile')(app);
 
 // socket.io
-require("./socket.io/socket")(app, PORT);
+require('./socket.io/socket')(app, PORT);
 
-require("./chat/chat")(app);
+require('./chat/chat')(app);
 
 // All remaining requests return the React app, so it can handle routing.
 app.use((req, res, next) => {
-    if (app.get("env") != "development")
-        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  if (app.get('env') !== 'development') res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 // app.listen(PORT, () => {
